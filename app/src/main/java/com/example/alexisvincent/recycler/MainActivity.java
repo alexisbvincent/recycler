077056ca6c1899ba5eea1693befce593b97dd2ca
AlexisVincent <avincent32@gatech.edu>
2018-10-28 16:51:30 -0400

    camera and firebase working

--- app/src/main/java/com/example/alexisvincent/recycler/MainActivity.java
+++ app/src/main/java/com/example/alexisvincent/recycler/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.alexisvincent.recycler;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -18,33 +23,39 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     private StorageReference mStorageRef;
+    private ProgressDialog mProgress;
+    private static int CAMERA_REQUEST_CODE = 1;
+    //private ImageView mImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        startActivity(intent);
-
         mStorageRef = FirebaseStorage.getInstance().getReference();
-        Uri file = Uri.fromFile(new File("/SD card/DCIM/Camera"));
-        StorageReference riversRef = mStorageRef.child("images/rivers.jpg");
-
-        riversRef.putFile(file)
-                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        // Get a URL to the uploaded content
-                        Task<Uri> downloadUrl = taskSnapshot.getStorage().getDownloadUrl();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        // Handle unsuccessful uploads
-                        // ...
-                    }
-                });
+        mProgress = new ProgressDialog(this);
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
 
 
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK){
+            mProgress.setMessage("Uploading Image...");
+            mProgress.show();
+            Uri uri = data.getData();
+
+            StorageReference filePath = mStorageRef.child("Photos").child(uri.getLastPathSegment());
+            filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    mProgress.dismiss();
+                    Toast.makeText(MainActivity.this, "Uploading Finished...", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 }
